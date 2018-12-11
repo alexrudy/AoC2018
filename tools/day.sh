66252dc4e7870ee3fd1ff958f18de26a8b4a6cfe
Alex Rudy <alex.rudy@gmail.com>
2018-12-10 20:20:41 -0800

    Fix generator script

--- tools/day.sh
+++ tools/day.sh
@@ -3,8 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/.."
 
 echo "Preparing for day ${day}"
 mkdir -p "$DIR/puzzles/${day}/"
-touch -p "$DIR/puzzles/${day}/input.txt"
+touch "$DIR/puzzles/${day}/input.txt"
 
 if [ ! -e "$DIR/src/puzzles/day$day.rs" ]; then
-    cp "$DIR/dayn.rs" "$DIR/src/puzzles/day$day.rs"
+    cp "$DIR/tools/dayn.rs" "$DIR/src/puzzles/day$day.rs"
+    echo "pub mod day${day};" >> "$DIR/src/puzzles/mod.rs"
 fi
+
