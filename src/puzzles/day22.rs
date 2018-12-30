810533ae428094af8dde470ca9a7aeabd8908a4c
Alex Rudy <alex.rudy@gmail.com>
2018-12-29 20:57:26 -0800

    Day 22 solution

--- src/puzzles/day22.rs
+++ src/puzzles/day22.rs
@@ -1,9 +1,10 @@
 use std::cell::RefCell;
-use std::collections::BTreeMap;
+use std::cmp;
+use std::collections::{BTreeMap, BinaryHeap};
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-use failure::{Error, Fail};
+use failure::{format_err, Error, Fail};
 
 use geometry::{BoundingBox, Direction, ParsePointError, Point, Position};
 
@@ -16,6 +17,9 @@ pub(crate) fn main() -> Result<(), Error> {
         cave.risk_level(BoundingBox::from_corners(Point::new(0, 0), cave.target))
     );
 
+    let minutes_to_rescue = shortest_path(&cave, Point::new(0, 0))
+        .ok_or_else(|| format_err!("No path found to target!"))?;
+    println!("Part 2: {}", minutes_to_rescue);
     Ok(())
 }
 
@@ -51,7 +55,7 @@ impl Region {
     }
 
     pub(crate) fn typ(self) -> Typ {
-        match self.erosion_index % 3 {
+        match self.erosion_index.abs() % 3 {
             0 => Typ::Rocky,
             1 => Typ::Wet,
             2 => Typ::Narrow,
@@ -170,6 +174,157 @@ impl FromStr for Cave {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum Equipment {
+    Nothing,
+    ClimbingGear,
+    Torch,
+}
+
+impl Equipment {
+    fn passable(self, terrain: Typ) -> bool {
+        match (self, terrain) {
+            (Equipment::Nothing, Typ::Rocky) => false,
+            (Equipment::Torch, Typ::Wet) => false,
+            (Equipment::ClimbingGear, Typ::Narrow) => false,
+            (_, _) => true,
+        }
+    }
+
+    fn change_gear(self, terrain: Typ) -> Self {
+        match (self, terrain) {
+            (Equipment::ClimbingGear, Typ::Rocky) => Equipment::Torch,
+            (Equipment::Torch, Typ::Rocky) => Equipment::ClimbingGear,
+            (Equipment::Nothing, Typ::Wet) => Equipment::ClimbingGear,
+            (Equipment::ClimbingGear, Typ::Wet) => Equipment::Nothing,
+            (Equipment::Torch, Typ::Narrow) => Equipment::Nothing,
+            (Equipment::Nothing, Typ::Narrow) => Equipment::Torch,
+            (_, _) => panic!(
+                "Illegal equipment / terrain combination: {:?}/{:?}",
+                self, terrain
+            ),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct PathFragment {
+    position: Point,
+    distance: usize,
+    heuristic: usize,
+    equipment: Equipment,
+}
+
+impl PathFragment {
+    fn new(position: Point, distance: usize, heuristic: usize, equipment: Equipment) -> Self {
+        Self {
+            position,
+            distance,
+            heuristic,
+            equipment,
+        }
+    }
+}
+
+impl cmp::Ord for PathFragment {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.heuristic
+            .cmp(&other.heuristic)
+            .reverse()
+            .then(self.position.cmp(&other.position))
+    }
+}
+
+impl cmp::PartialOrd for PathFragment {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug)]
+struct PathQueue {
+    queue: BinaryHeap<PathFragment>,
+    seen: BTreeMap<(Point, Equipment), usize>,
+}
+
+impl PathQueue {
+    fn new() -> Self {
+        Self {
+            queue: BinaryHeap::new(),
+            seen: BTreeMap::new(),
+        }
+    }
+
+    fn push(&mut self, path: PathFragment) {
+        match self.seen.get_mut(&(path.position, path.equipment)) {
+            Some(d) => {
+                if *d > path.distance {
+                    *d = path.distance;
+                    self.queue.push(path);
+                }
+            }
+            None => {
+                self.seen
+                    .insert((path.position, path.equipment), path.distance);
+                self.queue.push(path);
+            }
+        }
+    }
+
+    fn pop(&mut self) -> Option<PathFragment> {
+        self.queue.pop()
+    }
+}
+
+fn shortest_path(cave: &Cave, origin: Point) -> Option<usize> {
+    let path = PathFragment::new(
+        origin,
+        0,
+        origin.manhattan_distance(cave.target) as usize,
+        Equipment::Torch,
+    );
+    let mut queue = PathQueue::new();
+
+    queue.push(path);
+
+    while let Some(path) = queue.pop() {
+        if path.position == cave.target && path.equipment == Equipment::Torch {
+            return Some(path.distance);
+        }
+
+        let current_typ = cave.region(path.position).typ();
+
+        let change_equipment_path = PathFragment::new(
+            path.position,
+            path.distance + 7,
+            path.heuristic + 7,
+            path.equipment.change_gear(current_typ),
+        );
+        queue.push(change_equipment_path);
+
+        for adjacent in path.position.adjacent() {
+            if adjacent.x < 0 || adjacent.y < 0 {
+                continue;
+            }
+
+            let typ = cave.region(adjacent).typ();
+
+            if path.equipment.passable(typ) {
+                let adjacent_path = PathFragment::new(
+                    adjacent,
+                    path.distance + 1,
+                    path.distance + 1 + adjacent.manhattan_distance(cave.target) as usize,
+                    path.equipment,
+                );
+                queue.push(adjacent_path);
+            }
+        }
+    }
+
+    // No appropriate path found.
+    None
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -188,6 +343,13 @@ mod test {
             cave.risk_level(BoundingBox::from_corners(Point::new(0, 0), target)),
             114
         );
+    }
+
+    #[test]
+    fn example_part2() {
+        let cave = Cave::new(Point::new(10, 10), 510);
+
+        assert_eq!(shortest_path(&cave, Point::new(0, 0)), Some(45));
     }
 
 }
